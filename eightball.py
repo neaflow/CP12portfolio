ac71ac780074a5c875f976c5fcb969a323b8e63d
jj <james.bird011@gmail.com>
2026-09-11 11:01:08 -0700

    wont get last answer
    it won't do the same answer twice in a row

--- eightball.py
+++ eightball.py
@@ -1,5 +1,14 @@
 import random #we need this to select a random numbre
-q = input("what's your question: ") #literally no purpose
-whichans = random.randint(0, 7) #which answer index
-answers=["Nope.", "Yup.", "Maybe.", "Probably.", "ask later.", "secret ending", "Absolutely", "Absolutely not"] #possible naswers
-print(answers[whichans]) #print the answer from its index
+prevAns=0
+while True:
+    q = input("what's your question: ") #literally no purpose
+    gotNewAns = 0
+    while gotNewAns == 0:
+        whichansCurrent = random.randint(0, 7) #which answer index
+        
+        
+        answers=["Nope.", "Yup.", "Maybe.", "Probably.", "ask later.", "secret ending", "Absolutely", "Absolutely not"] #possible naswers
+        if whichansCurrent != prevAns:
+            print(answers[whichansCurrent]) #print the answer from its index
+            gotNewAns = 1
+            prevAns=whichansCurrent
