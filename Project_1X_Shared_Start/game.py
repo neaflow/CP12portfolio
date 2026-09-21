d77f209d523570e0cf1d8e8d8aab0329c8c65a28
jj <james.bird011@gmail.com>
2026-09-21 13:01:00 -0700

    added rooms and movement
    it's not fully functional though

--- Project_1X_Shared_Start/game.py
+++ Project_1X_Shared_Start/game.py
@@ -1,3 +1,4 @@
+import random
 health = 100
 damage = 10
 trait_health_damage = {
@@ -7,6 +8,9 @@ trait_health_damage = {
     "hydro": [2, 0.5],
     "electro": [3, 0.1],
 }
+rooms=[]
+numofrooms = random.randint(30, 50)
+
 print("-------------------------")
 print("base health is " + str(health))
 print("base damage is " + str(damage))
@@ -17,6 +21,8 @@ printed =1
 for i in trait_health_damage:
     print(str(printed) + ": " + i + " (health multiplier: " + str(trait_health_damage[i][0]) + ", damage multiplier: " + str(trait_health_damage[i][1]) + ")")
     printed = printed + 1
+
+
 
 
 def check_if_valid_trait(trait):
@@ -37,4 +43,71 @@ while True:
     else:
         print("not valid trait")
 
+roomsinrow = 8
+left = roomsinrow
+rowssofar=0
+
+inroom = random.randint(1, numofrooms)
+rooms=[]
+def printroomgrid():
+    print("#############################################")
+    for item in rooms:
+        if item == '\n':
+            print()
+        else:
+            if item < 10:
+                oneortwo = "0" + str(item)
+            else:
+                oneortwo = str(item)
+            # 2. Check if this is the room we are currently in
+            if item == inroom:
+                print("[" + oneortwo + "]", end=" ")  # Highlights current room: [05]
+            else:
+                print(" " + oneortwo + " ", end=" ")  # Highlights current room: [05]
+    print()
+    print("#############################################")
+for i in range(numofrooms):
+    rooms.append(i+1)
+    left=left-1
+    if (left == 0):
+        rooms.append("\n")
+        left = roomsinrow
+
+printroomgrid()
+
+
+leftedgenums=[]
+onleftedge=1
+rightedgenums=[]
+firstrightedge=roomsinrow
+onrightedge=roomsinrow
+while onleftedge<=numofrooms:
+    leftedgenums.append(onleftedge)
+    onleftedge = onleftedge + roomsinrow
+while onrightedge<=numofrooms:
+    rightedgenums.append(onrightedge)
+    onrightedge = onrightedge + roomsinrow
+
+print()
+print("you are in room #" + str(inroom))
+while True:
+    print(" ")
+    act=input("WASD to move forward left back right, \",\" to pickup: ")
+    if (act=="W"):
+        if(inroom-roomsinrow>0):
+            inroom =inroom-roomsinrow
+            printroomgrid()
+    if (act=="S"):
+        if(inroom+roomsinrow<numofrooms):
+            inroom =inroom+roomsinrow
+            printroomgrid()
+    if (act=="A"):
+        if(int(inroom) not in leftedgenums):
+            inroom =inroom-1
+            printroomgrid()
+    if (act=="D"):
+        if(int(inroom) not in rightedgenums):
+            inroom =inroom+1
+            printroomgrid()
+    
 
